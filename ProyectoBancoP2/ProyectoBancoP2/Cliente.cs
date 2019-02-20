b69d42852724b4a920f8c822eae7bf75e3aec739
Jorge Osuna <Jorgeosunaqui@gmail.com>
2019-02-20 00:12:02 -0700

    Vamos, prro

---
+++ ProyectoBancoP2/ProyectoBancoP2/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoBancoP2
+{
+    class Cliente
+    {
+        private static int count;
+        private int clave;
+        private string nombre;
+        private string domicilio;
+        private string ciudad;
+        private string telefono;
+
+        public Cliente(int clave, string nom, string dom, string ci, string tel)
+        {
+            count++;
+            this.clave = count;
+            this.nombre = nom;
+            this.domicilio = dom;
+            this.ciudad = ci;
+            this.telefono = tel;
+        }
+
+        public int pClave {
+            get => clave;
+        }
+        public string pNombre {
+            get => nombre;
+            set => nombre = value;
+        }
+        public string pDomicilio {
+            get => domicilio;
+            set => domicilio = value;
+        }
+        public string pCiudad {
+            get => ciudad;
+            set => ciudad = value;
+        }
+        public string pTelefono {
+            get => telefono;
+            set => telefono = value;
+        }
+
+        public override string ToString()
+        {
+            string res = String.Format("Nombre: {0,-5}\n Clave: {1,-3}\n Ciudad de residencia: {2,-3}, Domicilio: {3,-5}\n " +
+                "Teléfono: {4,-5}",pNombre,pClave,pCiudad,pDomicilio,pTelefono);
+
+            return res;
+        }
+    }
+}
---
+++ ProyectoBancoP2/ProyectoBancoP2/Cuenta.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoBancoP2
+{
+    class Cuenta
+    {
+        private static int count;
+        private int claveCuenta;
+        private double saldo;
+        private string nombreCuenta;
+        private int claveCliente;
+
+        public Cuenta(double saldoInicial,string tipoCuenta,int claveCliente)
+        {
+            this.claveCliente = claveCliente;
+            this.claveCuenta = count;
+            this.nombreCuenta = tipoCuenta;
+            this.saldo = saldoInicial;
+        }
+
+        public int pClaveCuenta
+        {
+            get => claveCuenta;
+        }
+        public double pSaldo
+        {
+            get => saldo;
+            set => saldo = value;
+        }
+        public string pNombre
+        {
+            get => nombreCuenta;
+            set => nombreCuenta = value;
+        }
+        public int pClaveCliente
+        {
+            get => claveCliente;
+            set => claveCliente = value;//?
+        }
+
+        public override string ToString()
+        {
+            string res = String.Format("Tipo de cuenta: {0,-5}\n Clave: {1,-3}\n, Clave del cliente asociado: {2,-3}\n " +
+                "Saldo actual: {3,-3}",pNombre,pClaveCuenta,pClaveCliente,pSaldo);
+
+            return res;
+        }
+    }
+}
