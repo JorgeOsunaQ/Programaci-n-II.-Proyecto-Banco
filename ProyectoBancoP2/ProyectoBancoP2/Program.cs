b3069a44715774e50a52feaaa8528347f6d8d746
Jorge Osuna <Jorgeosunaqui@gmail.com>
2019-02-19 17:15:37 -0700

    Jalese, Oliver

---
+++ ProyectoBancoP2/ProyectoBancoP2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoBancoP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funciona? v2");
+        }
+    }
+}
